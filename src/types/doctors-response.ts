a05f31408229063c377291eb216cadfc6197132b
X_V.neer <xv.neer@gmail.com>
2024-03-30 15:05:22 +0300

    build:generated types for data.json

---
+++ src/types/doctors-response.ts
@@ -0,0 +1,29 @@
+export type DoctorResponse = {
+  doctors: Doctor[]
+}
+
+export type Doctor = {
+  id: number
+  slug: string
+  personalInformation: PersonalInformation
+  skills: string[]
+}
+
+export type PersonalInformation = {
+  name: string
+  specialization: string
+  location: string
+  contact: Contact
+}
+
+export type Contact = {
+  phoneNumber: string
+  email: string
+  socialMedia: SocialMedia[]
+}
+
+export type SocialMedia = {
+  id: number
+  platform: string
+  user: string
+}
