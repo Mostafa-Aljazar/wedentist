80c950cd373cb2cbafa62293645425dd7870e1c8
X_V.neer <xv.neer@gmail.com>
2024-05-17 17:11:37 +0300

    feat: get doctor data from db

--- src/app/(public)/[slug]/layout.tsx
+++ src/app/(public)/[slug]/layout.tsx
@@ -1,7 +1,9 @@
-import DoctorCard from "@/components/doctor-card"
-import {type Doctor } from "@/models/Doctor"
 import React from "react"
-import data from "@/content/data/data.json"
+import { notFound } from "next/navigation"
+import Doctor, { type Doctor as DoctorType } from "@/models/Doctor"
+
+import dbConnect from "@/lib/db"
+import DoctorCard from "@/components/doctor-card"
 
 type Props = {
   children: React.ReactNode
@@ -9,23 +11,21 @@ type Props = {
     slug: string
   }
 }
-export const dynamicParams = false
-export async function generateStaticParams() {
-  return data.doctors.map((doctor: Doctor) => ({
-    slug: doctor.slug,
-  }))
-}
 
 export default async function layout({ children, params }: Props) {
   console.log(process.env.NEXT_PUBLIC_BASE_URL)
   const { slug } = params
-  const doctor = data.doctors.find((d) => d.slug === slug)!
+  await dbConnect()
+  const doctor = (await Doctor.findOne({ slug })) as DoctorType
+
+  !doctor && notFound()
+
   return (
     <main className=" py-8  ">
       <div className="container">
-        <div className=" md:flex gap-8">
+        <div className=" gap-8 md:flex">
           <div className="w-[220px] shrink-0 max-md:hidden">
-            <DoctorCard data={doctor} />
+            <DoctorCard data={JSON.parse(JSON.stringify(doctor))} />
           </div>
           {children}
         </div>
