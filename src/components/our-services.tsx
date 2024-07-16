74281b7d0f105f6854d027833e74f46c142581a3
X_V.neer <xv.neer@gmail.com>
2024-07-16 22:43:42 +0300

    style: fixed style

--- src/components/our-services.tsx
+++ src/components/our-services.tsx
@@ -8,53 +8,70 @@ const OurServices = (props: Props) => {
       <div className="container">
         <div className="flex items-center py-20">
           <div className="grow">
-            <div className="flex aspect-square w-fit items-center rounded-full bg-primary text-white">
-              <h2 className=" w-fit   text-5xl font-semibold ">خدماتنا</h2>
+            <div className="flex aspect-square w-fit items-center rounded-full bg-primary p-2 text-white">
+              <h2 className=" w-fit text-4xl   font-semibold lg:text-5xl ">
+                خدماتنا
+              </h2>
             </div>
-            <ul className="mt-16 grid grid-cols-1 gap-8  text-2xl  font-semibold md:grid-cols-2 lg:grid-cols-2">
-              <li className="relative before:right-0 before:ml-3 before:inline-block before:aspect-square before:w-5 before:translate-y-1 before:rounded-full before:bg-primary">
+            <ul className="mt-16 grid grid-cols-1 gap-8 text-lg font-semibold  md:grid-cols-2 lg:grid-cols-2 lg:text-2xl">
+              <li className="relative flex items-center gap-2">
+                <span className="right-0  block aspect-square w-5  shrink-0 rounded-full bg-primary"></span>
                 فيديوهات UGC
               </li>
-              <li className="relative before:right-0 before:ml-3 before:inline-block before:aspect-square before:w-5 before:translate-y-1 before:rounded-full before:bg-primary">
+              <li className="relative flex items-center gap-2">
+                <span className="right-0  block aspect-square w-5  shrink-0 rounded-full bg-primary"></span>
                 إدارة وتنسيق إعلانات المؤثرين
               </li>
-              <li className="relative before:right-0 before:ml-3 before:inline-block before:aspect-square before:w-5 before:translate-y-1 before:rounded-full before:bg-primary">
+              <li className="relative flex items-center gap-2">
+                <span className="right-0  block aspect-square w-5  shrink-0 rounded-full bg-primary"></span>
                 كتابة المحتوى الاعلاني
               </li>
-              <li className="relative before:right-0 before:ml-3 before:inline-block before:aspect-square before:w-5 before:translate-y-1 before:rounded-full before:bg-primary">
+              <li className="relative flex items-center gap-2">
+                <span className="right-0  block aspect-square w-5  shrink-0 rounded-full bg-primary"></span>
                 إدارة الحملات الإعلانية عن طريق المؤثرين
               </li>
-              <li className="relative before:right-0 before:ml-3 before:inline-block before:aspect-square before:w-5 before:translate-y-1 before:rounded-full before:bg-primary">
+              <li className="relative flex items-center gap-2">
+                <span className="right-0  block aspect-square w-5  shrink-0 rounded-full bg-primary"></span>
                 إدارة الحملات الإعلانية عن طريق الإعلانات الممولة
               </li>
-              <li className="relative before:right-0 before:ml-3 before:inline-block before:aspect-square before:w-5 before:translate-y-1 before:rounded-full before:bg-primary">
+              <li className="relative flex items-center gap-2">
+                <span className="right-0  block aspect-square w-5  shrink-0 rounded-full bg-primary"></span>
                 إدارة الحملات الإعلانية عن طريق الUGC
               </li>
-              <li className="relative before:right-0 before:ml-3 before:inline-block before:aspect-square before:w-5 before:translate-y-1 before:rounded-full before:bg-primary">
+              <li className="relative flex items-center gap-2">
+                <span className="right-0  block aspect-square w-5  shrink-0 rounded-full bg-primary"></span>
                 تصميم مواقع وصفحات الهبوط
               </li>
-              <li className="relative before:right-0 before:ml-3 before:inline-block before:aspect-square before:w-5 before:translate-y-1 before:rounded-full before:bg-primary">
+              <li className="relative flex items-center gap-2">
+                <span className="right-0  block aspect-square w-5  shrink-0 rounded-full bg-primary"></span>
                 توفير حلول تسويقية تقنية
               </li>
-              <li className="relative before:right-0 before:ml-3 before:inline-block before:aspect-square before:w-5 before:translate-y-1 before:rounded-full before:bg-primary">
+              <li className="relative flex items-center gap-2">
+                <span className="right-0  block aspect-square w-5  shrink-0 rounded-full bg-primary"></span>
                 دراسة وتحليل المشروع
               </li>
-              <li className="relative before:right-0 before:ml-3 before:inline-block before:aspect-square before:w-5 before:translate-y-1 before:rounded-full before:bg-primary">
+              <li className="relative flex items-center gap-2">
+                <span className="right-0  block aspect-square w-5  shrink-0 rounded-full bg-primary"></span>
                 تصميم واجهات المستخدم
               </li>
-              <li className="relative before:right-0 before:ml-3 before:inline-block before:aspect-square before:w-5 before:translate-y-1 before:rounded-full before:bg-primary">
+              <li className="relative flex items-center gap-2">
+                <span className="right-0  block aspect-square w-5  shrink-0 rounded-full bg-primary"></span>
                 تطوير النظام
               </li>
-              <li className="relative before:right-0 before:ml-3 before:inline-block before:aspect-square before:w-5 before:translate-y-1 before:rounded-full before:bg-primary">
+              <li className="relative flex items-center gap-2">
+                <span className="right-0  block aspect-square w-5  shrink-0 rounded-full bg-primary"></span>
                 اختبار النظام
               </li>
-              <li className="relative before:right-0 before:ml-3 before:inline-block before:aspect-square before:w-5 before:translate-y-1 before:rounded-full before:bg-primary">
+              <li className="relative flex items-center gap-2">
+                <span className="right-0  block aspect-square w-5  shrink-0 rounded-full bg-primary"></span>
                 إطلاق المشروع
               </li>
-              <li className="relative before:right-0 before:ml-3 before:inline-block before:aspect-square before:w-5 before:translate-y-1 before:rounded-full before:bg-primary">
+              <li className="relative flex items-center gap-2">
+                <span className="right-0  block aspect-square w-5  shrink-0 rounded-full bg-primary"></span>
                 صيانة المشروع
               </li>
-              <li className="relative before:right-0 before:ml-3 before:inline-block before:aspect-square before:w-5 before:translate-y-1 before:rounded-full before:bg-primary">
+              <li className="relative flex items-center gap-2">
+                <span className="right-0  block aspect-square w-5  shrink-0 rounded-full bg-primary"></span>
                 تطوير الالعاب
               </li>
             </ul>
